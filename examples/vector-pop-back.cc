80faa6e79eb5cc2e5a43196b35f6e767513a088f
Jon Craton <jncraton@gmail.com>
2026-09-21 11:42:24 -0400

    Add vector-pop-back.cc

---
+++ examples/vector-pop-back.cc
@@ -0,0 +1,17 @@
+#include <print>
+#include <vector>
+#include <chrono>
+
+int main() {
+  std::vector<int> v(1000000);
+  auto start = std::chrono::high_resolution_clock::now();
+
+  for (int i = 0; i < 1000000; ++i) {
+    v.pop_back();
+  }
+
+  auto end = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> diff = end - start;
+  std::println("Time: {}s", diff.count());
+  return 0;
+}
