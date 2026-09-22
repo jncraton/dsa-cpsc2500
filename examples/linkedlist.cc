b4340b43760303cfd1da300d295e648b7a038cef
Jon Craton <jncraton@gmail.com>
2026-09-22 10:57:36 -0400

    Add linkedlist.cc

---
+++ examples/linkedlist.cc
@@ -0,0 +1,45 @@
+#include <print>
+
+class Node {
+public:
+  int data;
+  Node* next;
+
+  Node(int data, Node* next = nullptr) : data(data), next(next) {}
+};
+
+void printList(Node* head) {
+  Node* current = head;
+  while (current != nullptr) {
+    std::print("{} -> ", current->data);
+    current = current->next;
+  }
+  std::println("nullptr");
+}
+
+int main() {
+  // Allocate nodes
+  Node* first = new Node(10);
+  Node* second = new Node(20);
+  Node* third = new Node(30);
+
+  // Connect nodes
+  first->next = second;
+  second->next = third;
+
+  // Traverse and print
+  Node* current = first;
+  while (current != nullptr) {
+    std::println("Node data: {}", current->data);
+    current = current->next;
+  }
+
+  printList(first);
+
+  // Clean up
+  delete third;
+  delete second;
+  delete first;
+
+  return 0;
+}
